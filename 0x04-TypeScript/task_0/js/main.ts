a3dc16c65eae8b7f0f12b2b780f85314739c2c94
claranceJ <jacktonec@gmail.com>
2024-12-14 15:19:24 +0300

    feat: configuring files and creating interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,42 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
+  
+  const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    location: 'New York'
+  };
+  
+  const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    age: 30,
+    location: 'Los Angeles'
+  };
+  
+  const studentsList: Student[] = [student1, student2];
+  
+  const table = document.createElement('table');
+  const tableBody = document.createElement('tbody');
+  table.appendChild(tableBody);
+  
+  studentsList.forEach(student => {
+    const row = document.createElement('tr');
+    const firstNameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+  
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+  
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+    tableBody.appendChild(row);
+  });
+  
+  document.body.appendChild(table);
+  
